d0fa2ae3d4e49e7b8e430fdd8a93400257d4cf6a
iusmac <iusico.maxim@libero.it>
2024-04-17 14:59:22 +0200

    Welcome to Android 14
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- builder.sh
+++ builder.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 bash ContainerizedAndroidBuilder/run.sh \
-    --android '13.0'  \
+    --android '14.0'  \
     --repo-url 'https://github.com/crdroidandroid/android.git' \
-    --repo-revision '13.0' \
+    --repo-revision '14.0' \
     --lunch-system 'lineage' \
     --lunch-device 'rova' \
     --lunch-flavor 'user' \
